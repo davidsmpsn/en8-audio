0c9a86e6ce7c46676b999422371367af49ce3dc5
David Simpson <davidsimpson@Davids-MBP.fritz.box>
2024-05-27 17:34:12 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `EN8 Audio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
