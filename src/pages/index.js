0fb4fdaae812aa951727ee920f06411bf0bda16b
David Simpson <davidsimpsoninbox@gmail.com>
2024-06-09 16:35:32 +0100

    Updated design

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,53 +42,125 @@ const IndexPage = () => {
     'umg'
   ]
 
+  const projects = [
+    {
+      name: 'Rex Orange County',
+      description: 'Description',
+      img: 'rex'
+    },
+    {
+      name: 'Shyfoot',
+      description: 'Live Session',
+      img: 'shyfoot'
+    },
+    {
+      name: 'Jamie T',
+      description: 'Live at Finsbury Park',
+      img: 'jamie-t'
+    },
+    {
+      name: 'Heading',
+      description: 'Description',
+      img: 'fender'
+    },
+    {
+      name: 'Heading',
+      description: 'Description',
+      img: 'koko'
+    },
+    {
+      name: 'Heading',
+      description: 'Description',
+      img: 'kiwanuka'
+    },
+    {
+      name: 'Heading',
+      description: 'Description',
+      img: 'rex'
+    },
+    {
+      name: 'Heading',
+      description: 'Description',
+      img: 'rex'
+    },
+    {
+      name: 'Heading',
+      description: 'Description',
+      img: 'rex'
+    },
+    {
+      name: 'Heading',
+      description: 'Description',
+      img: 'rex'
+    }
+  ]
+
   return (
-    <div className='wrapper'>
-      
-      <div className='header'>
-        <h1><a href=''>EN8 AUDIO</a></h1>
-        <div className='header__nav'>
-          <ul>
-            <li><a href=''>Contact</a></li>
-          </ul>
+    <div>
+      <div className="page_top">
+        <div className="wrapper">
+        
+          <div className="header">
+            <h1><a href="">EN8 Audio</a></h1>
+            <div className="header__nav">
+              <ul>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Clients</a></li>
+                <li><a href="">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="hero">
+            <div className="hero__inner">
+              <h2>
+                <span>Audio recording, mixing and production </span>
+                <span>in the commercial & creative domain.</span>
+              </h2>
+
+              <a href="contact" className="button">Get In Touch</a>
+            </div>
+          </div>
+        
         </div>
       </div>
 
-      <div className='hero'>
-        <div className='hero__inner'>
-          <h2>Setting a new standard in audio recording, mixing and production services in the commercial & creative domain.</h2>
+      <div className="wrapper">
+
+        <div className="about">
+          <p><span>London based</span>, we are a team with over <span>30 years experience</span> in the field.</p>
+          <p>Working across <span>location sound</span>, <span>audio post-production</span> and <span>commercial recorded music</span>; trusted by some of the <span>biggest recording artists</span> and most <span>established production companies alike</span></p>
         </div>
-      </div>
 
-      <div className='description1'>
-        <p>We are an audio production house delivering the highest quality audio services on the market. London based, we are a team of over 30 years experience in the field with a wide range of experience in Video & TV production & in the commercial recorded music domain; trusted by some of the biggest recording artists and most established production companies alike.</p>
-      </div>
-
-      <div className='description2'>
-        <p>Through open and easy communication, we offer reliable quality over a range of services that include but are not limited to; Location Recording such as filmed live sessions & acoustic sessions,  In-House Recording at our production house for high quality voiceover recording, and External Engineering services engineering any type of session at any high end recording studio (we’ve worked in them all).  Post-Production such as podcast editing, TV & Video production and Audio Repair services, and Mixing; we’re very well equipped to offer mix services of all kinds at a very high level.</p>
-      </div>
-
-      <div className='projects'>
-
-      </div>
-
-      <div className='clients'>
-        <div className='clients__list'>
-        {music.map(client => (
-            <div className='clients__client'>
-              <img src={`./client-logos/music/${client}.svg`} alt='client' />
+        <div className="projects">
+          {projects.map(project => (
+            <div className="projects__project">
+              <a href="">
+                <img src={`./projects/${project.img}.png`} alt={project.name} />
+                <h3><span>{project.name}</span> // {project.description}</h3>
+              </a>
             </div>
           ))}
         </div>
-        <div className='clients__list'>
-          {companies.map(client => (
-            <div className='clients__client'>
-              <img src={`./client-logos/companies/${client}.svg`} alt='client' />
-            </div>
-          ))}
+
+        <div className="clients">
+          <div className="clients__list">
+          {music.map(client => (
+              <div className="clients__client">
+                <img src={`./client-logos/music/${client}.svg`} alt="client" />
+              </div>
+            ))}
+          </div>
+          <div className="clients__list">
+            {companies.map(client => (
+              <div className="clients__client">
+                <img src={`./client-logos/companies/${client}.svg`} alt="client" />
+              </div>
+            ))}
+          </div>
         </div>
+        
       </div>
-      
     </div>
   )
 }
