f686c0c27c5f0fec48223546f78a6c583918dc6b
David Simpson <davidsimpsoninbox@gmail.com>
2024-09-11 19:07:42 +0100

    Fixed projects slider

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,45 +1,75 @@
-import React, { useState } from 'react'
-import { motion, useMotionValue } from 'framer-motion'
+import React, { useState, useEffect } from 'react'
+import { motion, useMotionValue, useAnimation } from 'framer-motion'
 import useMeasure from 'react-use-measure'
 import { GatsbyImage } from 'gatsby-plugin-image'
-import { PortableText } from '@portabletext/react'
 
 export const Projects = ({ projects, text }) => {
   const [imgIndex, setImgIndex] = useState(0)
 
-  const dragX = useMotionValue(0)
+  const x = useMotionValue(0)
+  const controls = useAnimation()
 
-  let [ref, { width }] = useMeasure()
+  let [ref, { width: projectWidth }] = useMeasure()
 
-  const onDragEnd = () => {
-    const x = dragX.get()
+  const onDragEnd = (event, info) => {
+    const velocity = info.velocity.x
+    const finalPosition = x.get() + velocity / 2
+    const closestIndex = findClosestProject(finalPosition, projectWidth)
+    const targetX = -(closestIndex * projectWidth - closestIndex * -32)
 
-    if (x <= -50 && imgIndex < projects.length - 1) {
-      setImgIndex((pv) => pv + 1)
-    } else if ( x >= 50 && imgIndex > 0) {
-      setImgIndex((pv) => pv - 1)
-    }
+    controls.start({
+      x: targetX,
+      transition: {
+        type: 'spring',
+        stiffness: 300,
+        damping: 30,
+      }
+    })
+
+    setImgIndex(closestIndex)
   }
+
+  const findClosestProject = (finalPosition, containerWidth) => {
+    const offsets = projects.map((_, index) => {
+      return Math.abs(finalPosition + index * containerWidth)
+    })
+
+    return offsets.indexOf(Math.min(...offsets))
+  }
+
+  const handleNavClick = (direction) => {
+    let newIndex = imgIndex + direction
+    newIndex = Math.max(0, Math.min(newIndex, projects.length - 1))
+
+    const targetX = -(newIndex * projectWidth - newIndex * -32)
+
+    console.log(targetX)
+
+    controls.start({
+      x: targetX,
+      transition: {
+        type: 'spring',
+        stiffness: 300,
+        damping: 30,
+      }
+    })
+
+    setImgIndex(newIndex)
+  }
+
+
 
   return (
     <div className="projects">
       <div className="wrapper">
-        {/* <div className="projects__top">
-          <div className="projects__text">
-            <PortableText value={text} />
-          </div>
-
-          
-        </div> */}
-
         <div className="projects__nav">
-          <button onClick={() => imgIndex > 0 ? setImgIndex((pv) => pv - 1) : null}>
+          <button onClick={() => handleNavClick(-1)} disabled={imgIndex === 0}>
             <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#FFFFFF" aria-labelledby="left">
               <title id="left">Left</title>
               <path d="m287-446.67 240 240L480-160 160-480l320-320 47 46.67-240 240h513v66.66H287Z"/>
             </svg>
           </button>
-          <button onClick={() => imgIndex < projects.length - 1 ? setImgIndex((pv) => pv + 1 ) : null}>
+          <button onClick={() => handleNavClick(1)} disabled={imgIndex === projects.length - 1}>
           <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#FFFFFF" aria-labelledby="right">
             <title id="right">Right</title>
             <path d="M673-446.67H160v-66.66h513l-240-240L480-800l320 320-320 320-47-46.67 240-240Z"/>
@@ -50,24 +80,15 @@ export const Projects = ({ projects, text }) => {
         <motion.div
           className="projects__inner"
           drag="x"
-          dragConstraints={{
-            left: 0,
-            right: 0
-          }}
-          dragTransition={{ bounceStiffness: 100, bounceDamping: 30 }}
-          dragElastic={1}
-          style={{
-            x: dragX,
-            
-          }}
-          animate={{
-            translateX: `-${imgIndex * width + imgIndex * 32}px`
-          }}
+          dragConstraints={{ right: 0 }}
+          dragElastic={false}
+          style={{ x }}
+          animate={controls}
           onDragEnd={onDragEnd}
         >
           {projects.map(project => (
             <div className="projects__project" ref={ref} key={project.id}>
-              <a  target="_blank" rel="noreferrer" draggable="false">
+              <a href={project.link} target="_blank" rel="noreferrer" draggable="false">
                 <GatsbyImage
                   image={project.image.asset.gatsbyImageData}
                   alt={project.client + ' - ' + project.project}
