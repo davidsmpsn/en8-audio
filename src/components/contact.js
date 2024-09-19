b5293e342a73eb6ef3b44c9e40aa0ebdba5e6a7c
David Simpson <davidsimpsoninbox@gmail.com>
2024-09-19 12:41:12 +0100

    Text now copiable from clicking email address and removed dotted border

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
 export const Contact = ({ isOpen, handleOpen }) => {
+  const [copyStatus, setCopyStatus] = useState('Copy')
 
-  const copy = (e) => {
-    navigator.clipboard.writeText('enquiries@playheadaudio.com')
-    e.target.innerText = 'Copied!'
+  const copyToClipboard = (text, e) => {
+    navigator.clipboard.writeText(text)
+    setCopyStatus('Copied!')
+    setTimeout(() => setCopyStatus('Copy'), 2000)
   }
 
   return (
@@ -14,18 +16,26 @@ export const Contact = ({ isOpen, handleOpen }) => {
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: .5 }}
+          transition={{ duration: 0.5 }}
           exit={{ opacity: 0 }}
           className="overlay"
         >
           <div className="overlay__text">
             <div className="overlay__clipboard">
-              <h3>enquiries@playheadaudio.com</h3>
-              <button onClick={copy}>Copy</button>
+              <h3 
+                onClick={(e) => copyToClipboard('enquiries@playheadaudio.com', e)} 
+                style={{ cursor: 'pointer' }}
+              >
+                enquiries@playheadaudio.com
+              </h3>
+              <button onClick={() => copyToClipboard('enquiries@playheadaudio.com')}>
+                {copyStatus}
+              </button>
             </div>
           </div>
           <div className="overlay__trigger" onClick={() => handleOpen(false)}></div>
         </motion.div>
       )}
     </AnimatePresence>
-)}
+  )
+}
