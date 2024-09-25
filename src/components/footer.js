c6166a77b8851022666933718fca42a11f117b96
David Simpson <davidsimpsoninbox@gmail.com>
2024-09-25 11:46:23 +0100

    Adjusted footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,12 +11,12 @@ export const Footer = ({ handleOpen, text }) => (
         <button className="button button-sm" onClick={handleOpen}>Get in touch</button>
       </div>
       <div className="footer__section">
+        <p>&copy; Playhead Audio 2024</p>
         <p><a href="mailto:enquiries@playheadaudio.com">enquiries@playheadaudio.com</a></p>
         <div className="footer__socials">
           <a href="https://www.instagram.com/playheadaudio/" className="icon-instagram">Instagram</a>
           <a href="https://www.linkedin.com/company/playheadaudio/" className="icon-linkedin">LinkedIn</a>
         </div>
-        <p>&copy; Playhead Audio 2024</p>
       </div>
     </div>
   </footer>
